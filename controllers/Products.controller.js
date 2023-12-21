dc1ca1bd4c05b43949b8022bc853b6fc7b01534b
HassanTheProgrammer <144118637+HassanTheProgrammer@users.noreply.github.com>
2023-12-21 12:49:56 +0500

    Searching APIs complete

--- controllers/Products.controller.js
+++ controllers/Products.controller.js
@@ -15,7 +15,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields, numbericFilters } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -26,11 +26,28 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  if (numbericFilters) {
-    //TODO
-    console.log(numbericFilters);
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
-  //   console.log(queryObject);
+  console.log(queryObject);
   let result = Product.find(queryObject);
   /** Sort */
   if (sort) {
